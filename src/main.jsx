7c79aec898af2fb5c23a084a0ab811177f9127c6
Văn Quý <vivanquy20804@gmail.com>
2025-07-17 00:07:41 +0700

    video 43

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,40 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import App from './App';
 
+// Router định nghĩa đường dẫn
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/login",
+    element: <div>login page</div>
+  },
+  {
+    path: "/register",
+    element: <div>register page</div>
+  }
+  ,
+  {
+    path: "/users",
+    element: <div>users page</div>
+  }
+  ,
+  {
+    path: "/products",
+    element: <div>products page</div>
+  }
+]);
+
+// Render
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
